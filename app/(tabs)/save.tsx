967726d45454b8640f0a5a1643e56e787ada2488
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-03-22 22:50:31 +0500

    initial commit

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function TabsLayout() {
+export default function save() {
   return (
     <View>
-      <Text>_layout</Text>
+      <Text>save</Text>
     </View>
   )
 }
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function TabsLayout() {
+export default function search() {
   return (
     <View>
-      <Text>_layout</Text>
+      <Text>search</Text>
     </View>
   )
 }
