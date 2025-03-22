a991e2e2f605b1e28411d09ca4e0c9d3f0bd9790
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-03-23 03:19:07 +0500

    fetch request call

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,12 +1,33 @@
 export const TMDB_CONFIG = {
-    BASE_URL: "https://api.themoviedb.org/3",
-    API_KEY: process.env.TMDB_MOVIE_API_KEY,
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_MOVIE_API_KEY}`
-    }
-}
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.TMDB_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.TMDB_MOVIE_API_KEY}`,
+  },
+};
 
-export async function getMovies() {
-    
+export async function fetchMovies(query: string): Promise<Movie[]> {
+  try {
+    const endpoint = query
+      ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+          query
+        )}`
+      : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movies: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.results;
+  } catch (e) {
+    console.log("Error fetching movies", e);
+    return [];
+  }
 }
