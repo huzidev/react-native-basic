536d2734a9761a551353bc414bff113be5bcfb54
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-03-23 01:54:11 +0500

    tabs navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,19 +3,45 @@ import { Tabs } from 'expo-router'
 import TabIcon from '@/app/components/layout/TabIcon'
 import { icons } from '@/constants/icons'
 
+const tabs = [
+  {
+    name: 'index',
+    title: 'Home',
+    icon: icons.home,
+  },
+  {
+    name: 'search',
+    title: 'Search',
+    icon: icons.search,
+  },
+  {
+    name: 'save',
+    title: 'Save',
+    icon: icons.save,
+  },
+  {
+    name: 'profile',
+    title: 'Profile',
+    icon: icons.person,
+  },
+]
+
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen 
-        name="index"
-        options={{
-          title: 'index',
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <TabIcon focused={focused} icon={icons.home} title="Home" />
-          )
-        }}
-      />
+      {tabs.map(({ name, title, icon }) => (
+        <Tabs.Screen
+          key={name}
+          name={name}
+          options={{
+            title: title,
+            headerShown: false,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={icon} title={title} />
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   )
 }
