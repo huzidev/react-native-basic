344570d1b207d86f9e9b157624c2120726a420ed
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-03-22 23:02:04 +0500

    assets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
 export default function TabsLayout() {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen 
+        name="index"
+        options={{
+          title: 'index',
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+
+          )
+        }}
+      />
+    </Tabs>
   )
 }
