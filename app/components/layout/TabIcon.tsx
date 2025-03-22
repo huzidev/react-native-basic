3edb9cd1269e3cad77cecf43bae1303b79260fa0
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-03-23 01:46:44 +0500

    tab icons updated

--- app/components/layout/TabIcon.tsx
+++ app/components/layout/TabIcon.tsx
@@ -3,18 +3,19 @@ import { ImageBackground, Image, Text, View } from "react-native";
 
 export default function TabIcon({ focused, icon, title }: any) {
   return (
-    focused ? (
-        <ImageBackground 
-            source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-            >
-            <Image source={icon} tintColor="#151312" className="size-5" />
-            <Text>{title}</Text>
-        </ImageBackground>
-        ) : (
-        <View>
-            <Image source={icon} />
-        </View>
-    )
-  );
+  focused ? (
+    <ImageBackground
+      source={images.highlight}
+      className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+    >
+      <Image source={icon} tintColor="#151312" className="size-5" />
+      <Text className="text-secondary text-base font-semibold ml-2">
+        {title}
+      </Text>
+    </ImageBackground>
+  ) : (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  ));
 }
