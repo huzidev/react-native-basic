7edc098e0edca6a2ac4cc79063073f249b741469
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-01 22:52:54 +0500

    movie card styles

--- components/movie/MovieCard.tsx
+++ components/movie/MovieCard.tsx
@@ -14,29 +14,37 @@ export default function MovieCard({
   return (
     <Link href={`/movie/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
-        {/* Update tailwind config for image */}
-        <Image 
+        {/* Update tailwind config for images */}
+        <Image
           source={{
-            uri: poster_path 
+            uri: poster_path
               ? `https://image.tmdb.org/t/p/w500${poster_path}`
-              : "https://placehold.it/500x750.png?text=No+Image+Available"
+              : "https://placehold.it/500x750.png?text=No+Image+Available",
           }}
-          className='w-full h-52 rounded-lg'
+          className="w-full h-52 rounded-lg"
           resizeMode="cover"
         />
-        
-        <Text className='text-sm font-bold text-white mt-2'>
+
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
           {title}
         </Text>
 
-        <View className='flex-row items-center justify-between gap-x-1'>
-          <Image source={icons.star} className='size-4' />
-          <Text className='text-white text-xs font-bold'>
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-white text-sm font-bold">
             {Math.round(vote_average / 2)}
           </Text>
         </View>
-        
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-white text-xs font-bold mt-1">
+            {release_date}
+          </Text>
+          <Text className="text-xs font-medium text-light-300 uppercase">
+            Movie
+          </Text>
+        </View>
       </TouchableOpacity>
     </Link>
-  )
+  );
 }
