7d4b49f3b8e084506e9fd584d015e4f631327a81
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-02 21:34:49 +0500

    update query search

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,15 +1,21 @@
-import { Client, Databases } from "react-native-appwrite";
+import { Client, Databases, Query } from "react-native-appwrite";
 
-
-const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
-const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
+const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
 const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!;
-const ENDPOINT = 'https://cloud.appwrite.io/v1';
+const ENDPOINT = "https://cloud.appwrite.io/v1";
 
-
-const client = new Client()
-    .setEndpoint(ENDPOINT)
-    .setProject(PROJECT_ID)
-
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
 
 const database = new Databases(client);
+
+export async function updateSearchCount(query: string) {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+  } catch (err) {
+    console.log("Error updating search count", err);
+    throw new Error("Failed to update search count");
+  }
+}
