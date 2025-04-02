729b5ee0ef2addb41fc4a4b63343ba16370ed3c0
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-02 18:46:17 +0500

    search movie queries

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,11 @@
-import { View, Text, Image, FlatList } from 'react-native'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import { images } from '@/constants/images'
 import useFetch from '@/hooks/useFetch'
 import { fetchMovies } from '@/services/api';
 import MovieCard from '@/components/movie/MovieCard';
+import { icons } from '@/constants/icons';
+import SearchBar from '@/components/general/SearchBar';
 
 export default function search() {
   const [searchQuery, setSearchQuery] = useState<string>('');
@@ -16,19 +18,54 @@ export default function search() {
   } = useFetch(() => fetchMovies(searchQuery), false); // default auto-fetch is false only run when user searches
 
   return (
-    <View className='flex-1 bg-primary'>
-      <Image 
+    <View className="flex-1 bg-primary">
+      <Image
         source={images.bg}
-        className='flex-1 absolute w-full z-0'
-        resizeMode='cover'
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
       />
 
-      <FlatList 
-        data={data} 
+      <FlatList
+        data={data}
         renderItem={({ item }) => <MovieCard {...item} />}
         keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
 
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={(v: string) => setSearchQuery(v)}
+              />
+            </View>
+
+            {loading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+
+            {error && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {error.message}
+              </Text>
+            )}
+
+            {!loading && !error && searchQuery.trim() && !!data?.length && (
+              <Text className='text-xl text-white font-bold px-5 my-3'>
+                Search results for "{searchQuery}"
+              </Text>
+            )}
+          </>
+        }
       />
     </View>
-  )
+  );
 }
