9c9b6517136b2301c0fa0c2893f1e94d48e61f5b
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-02 18:35:31 +0500

    on query update

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
 
 export default function search() {
   return (
-    <View>
-      <Text>search</Text>
+    <View className='flex-1 bg-primary'>
+      <Image 
+        source={images.bg}
+        className='flex-1 absolute w-full z-0'
+        resizeMode='cover'
+      />
     </View>
   )
 }
