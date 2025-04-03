35b2aa11d09a3e23135f87ab35c879f32282e860
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-03 13:58:35 +0500

    app write movie update

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from "react-native-appwrite";
+import { Client, Databases, ID, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
@@ -9,11 +9,38 @@ const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
 
 const database = new Databases(client);
 
-export async function updateSearchCount(query: string) {
+export async function updateSearchCount(query: string, movie: Movie) {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
       Query.equal("searchTerm", query),
     ]);
+
+
+    if (!!result?.documents.length) {
+      const existingMovie = result.documents[0];
+
+      console.log("what is existingMovie", existingMovie);
+
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1
+        }
+      );
+    } else {
+      const response = await database.createDocument( DATABASE_ID, COLLECTION_ID, ID.unique(), {
+          searchTerm: query,
+          movie_id: movie?.id,
+          title: movie?.title,
+          count: 1,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+        }
+      )
+      console.log("What is response on movie create", response);
+    }
+
   } catch (err) {
     console.log("Error updating search count", err);
     throw new Error("Failed to update search count");
