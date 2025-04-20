04c86360ab64ace0b95a38f7a026997b1b19766b
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-20 17:08:02 +0500

    movie details - repeated

---
+++ components/movie/MovieInfo.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MovieInfo() {
+  return (
+    <View className='flex-col items-start justify-center mt-5'>
+        
+    </View>
+  )
+}
