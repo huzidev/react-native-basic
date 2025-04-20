a8b415ab5bbec13a0ba5fffcea408b022b843528
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-20 19:10:10 +0500

    on loading state

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router';
+import useFetch from '@/hooks/useFetch';
+import { fetchMovieDetails } from '@/services/api';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function MovieDetails() {
   const router = useRouter();
   const { id } = useLocalSearchParams();
+
+  const { data, loading, error } = useFetch(() => fetchMovieDetails(id as string));
+
+  if (loading) {
+    return (
+      <SafeAreaView className='bg-primary flex-1'>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
 
   return (
     <View>
