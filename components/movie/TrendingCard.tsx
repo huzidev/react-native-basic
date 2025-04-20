f37849eab9d863077deb81d2b02545fc00b13674
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-20 16:57:06 +0500

    indent

--- components/movie/TrendingCard.tsx
+++ components/movie/TrendingCard.tsx
@@ -1,34 +1,39 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { Link } from 'expo-router'
+import { Link } from "expo-router";
 import MaskedView from "@react-native-masked-view/masked-view";
 
 interface TrendingCardProps {
-    movieId: number
-    title: string
-    posterUrl: string
-    index: number
+  movieId: number;
+  title: string;
+  posterUrl: string;
+  index: number;
 }
 
-export default function TrendingCard({ movieId, title, posterUrl, index }: TrendingCardProps) {
+export default function TrendingCard({
+  movieId,
+  title,
+  posterUrl,
+  index,
+}: TrendingCardProps) {
   return (
     <Link href={`/movie/${movieId}`} asChild>
-        <TouchableOpacity className='w-32 relative pl-5'>
-            <Image 
-                source={{ uri: posterUrl }}
-                className="w-32 h-48 rounded-lg"
-                resizeMode="cover"
-            />
+      <TouchableOpacity className="w-32 relative pl-5">
+        <Image
+          source={{ uri: posterUrl }}
+          className="w-32 h-48 rounded-lg"
+          resizeMode="cover"
+        />
 
-            <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-                <MaskedView
-                    maskElement={
-                        <Text className="font-bold text-white text-6xl">
-                            {index + 1}
-                        </Text>
-                    }
-                    ></MaskedView>
-            </View>
-        </TouchableOpacity>
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            
+          </MaskedView>
+        </View>
+      </TouchableOpacity>
     </Link>
-  )
+  );
 }
