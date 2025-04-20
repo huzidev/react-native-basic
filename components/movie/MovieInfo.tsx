1f793c7e55d699c6aeccd12d231ee4c75bf23cca
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-04-20 17:10:07 +0500

    info component updated

--- components/movie/MovieInfo.tsx
+++ components/movie/MovieInfo.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function MovieInfo() {
+interface MovieInfoProps {
+  label: string;
+  value: string;
+}
+
+export default function MovieInfo({ label, value }: MovieInfoProps) {
   return (
-    <View className='flex-col items-start justify-center mt-5'>
-        
+    <View className="flex-col items-start justify-center mt-5">
+      <Text className="text-light-200 text-sm font-normal">{label}</Text>
+      <Text className="text-light-100 font-bold text-sm mt-2">
+        {value || "N/A"}
+      </Text>
     </View>
-  )
+  );
 }
